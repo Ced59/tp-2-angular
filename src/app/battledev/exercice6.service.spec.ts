ea43f38caff1781931a37071e7c94d458811ef0d
Cedric <c.caudron59@gmail.com>
2020-11-15 19:34:05 +0100

    prepare ex 6

--- src/app/battledev/exercice6.service.spec.ts
+++ src/app/battledev/exercice6.service.spec.ts
@@ -1,16 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { Exercice6Service } from './exercice6.service';
+import {Exercice5Service} from './exercice5.service';
+import {ParsingService} from './parsing/parsing.service';
+import {instance, mock} from 'ts-mockito';
 
 describe('Exercice6Service', () => {
-  let service: Exercice6Service;
+  let exercice6Service: Exercice6Service;
+  let mockParsingService: ParsingService;
+  let parsingService: ParsingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(Exercice6Service);
+    mockParsingService = mock(ParsingService);
+    parsingService = instance(mockParsingService);
+    exercice6Service = new Exercice6Service(parsingService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+
 });
--- src/app/battledev/exercice6.service.ts
+++ src/app/battledev/exercice6.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {ParsingService} from './parsing/parsing.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Exercice6Service {
+  parsingService: ParsingService;
+  inputString: string[];
 
-  constructor() { }
+  constructor(parsingService: ParsingService) {
+    this.parsingService = parsingService;
+    this.inputString = [];
+  }
 }
