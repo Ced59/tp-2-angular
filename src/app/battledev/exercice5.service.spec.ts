a4e1a7e59b25f9ae4b69f817c08167206860fe7d
Cedric <c.caudron59@gmail.com>
2020-11-15 18:40:34 +0100

    TDD tests divisible

--- src/app/battledev/exercice5.service.spec.ts
+++ src/app/battledev/exercice5.service.spec.ts
@@ -1,16 +1,71 @@
 import { TestBed } from '@angular/core/testing';
 
 import { Exercice5Service } from './exercice5.service';
+import {ParsingService} from './parsing/parsing.service';
+import {instance, mock} from 'ts-mockito';
 
 describe('Exercice5Service', () => {
-  let service: Exercice5Service;
+  let exercice5Service: Exercice5Service;
+  let mockParsingService: ParsingService;
+  let parsingService: ParsingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(Exercice5Service);
+    mockParsingService = mock(ParsingService);
+    parsingService = instance(mockParsingService);
+    exercice5Service = new Exercice5Service(parsingService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should be divisible by 4', () => {
+    const year = 1680;
+    const expectedValue = true;
+
+    const result = exercice5Service.isDivBy4(year);
+
+    expect(result).toEqual(expectedValue);
+  });
+
+  it('should not be divisible by 4', () => {
+    const year = 1681;
+    const expectedValue = false;
+
+    const result = exercice5Service.isDivBy4(year);
+
+    expect(result).toEqual(expectedValue);
+  });
+
+  it('should be divisible by 400', () => {
+    const year = 1680;
+    const expectedValue = false;
+
+    const result = exercice5Service.isDivBy400(year);
+
+    expect(result).toEqual(expectedValue);
+  });
+
+  it('should be divisible by 400', () => {
+    const year = 2000;
+    const expectedValue = true;
+
+    const result = exercice5Service.isDivBy400(year);
+
+    expect(result).toEqual(expectedValue);
+  });
+
+  it('should be divisible by 100', () => {
+    const year = 1680;
+    const expectedValue = false;
+
+    const result = exercice5Service.isDivBy100(year);
+
+    expect(result).toEqual(expectedValue);
+  });
+
+  it('should be divisible by 100', () => {
+    const year = 1600;
+    const expectedValue = true;
+
+    const result = exercice5Service.isDivBy100(year);
+
+    expect(result).toEqual(expectedValue);
   });
 });
--- src/app/battledev/exercice5.service.ts
+++ src/app/battledev/exercice5.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {ParsingService} from './parsing/parsing.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Exercice5Service {
+  parsingService: ParsingService;
+  inputString: string[];
 
-  constructor() { }
+  constructor(parsingService: ParsingService) {
+    this.parsingService = parsingService;
+    this.inputString = [];
+  }
+
+  isDivBy4(year: number): boolean {
+    return false;
+  }
+
+  isDivBy400(year: number): boolean {
+    return false;
+  }
+
+  isDivBy100(year: number): boolean {
+    return false;
+  }
 }
